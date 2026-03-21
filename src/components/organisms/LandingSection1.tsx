de8489d1d6827ad52900dd1b45c1f6f1af84b677
jomoartz <johnjohananmogbolu@gmail.com>
2026-03-21 14:12:16 +0100

    feat: Landing section 1

---
+++ src/components/organisms/LandingSection1.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import { Text } from "../atoms/Text";
+import { Button } from "../molecules/Button";
+import FoodStack from "../molecules/FoodStack";
+
+const LandingSectionOne = () => {
+  return (
+    <main className="flex flex-col p-5 md:p-10 pt-32 md:pt-32 gap-10 justify-center items-center">
+      <div className="flex flex-col justify-center items-center">
+        <Text variant="hero-heading" color="orange">
+          Your Personal AI Market <br className="hidden md:flex" /> Assistant.
+        </Text>
+
+        <Text variant="hero-subtext" className="mt-5">
+          Anything you need, delivered <br className="md:hidden" />
+          right on WhatsApp.
+        </Text>
+        <div className="pt-5">
+          <Button
+            variant="gradient"
+            text="Start Ordering"
+            icon={
+              <Image
+                alt="whatsapp icon"
+                src="/icon_whatsapp.svg"
+                width={20}
+                height={20}
+              />
+            }
+          />
+        </div>
+      </div>
+      <FoodStack />
+    </main>
+  );
+};
+
+export { LandingSectionOne };
