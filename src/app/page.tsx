ec468ad6e3637fe5a6fcdb613e3a9838a6a3e1f5
jomoartz <johnjohananmogbolu@gmail.com>
2026-03-21 14:12:45 +0100

    feat: updated Landing screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,11 @@
-import { Text } from "@/components/atoms/Text";
+import { LandingSectionOne } from "@/components/organisms/LandingSection1";
+
+import { BackgroundOne } from "@/components/templates/BackgroundOne";
 
 export default function Home() {
   return (
-    <main className="p-10 space-y-10">
-
-      <Text variant="hero-heading" color="orange">
-        Your Personal AI Market Assistant
-      </Text>
-
-      <Text variant="hero-subtext">
-        Anything you need, delivered right on WhatsApp.
-      </Text>
-
-      <Text variant="section-heading-96" color="orange">
-        What You Can Order
-      </Text>
-
-      <Text variant="section-heading-48">
-        Fresh Groceries Delivered Fast
-      </Text>
-
-      <Text variant="cta-label" color="green">
-        Start Ordering Now
-      </Text>
-
-      <Text variant="cta-sublabel">
-        It only takes a few seconds
-      </Text>
-
-    </main>
+    <BackgroundOne>
+      <LandingSectionOne />
+    </BackgroundOne>
   );
 }
