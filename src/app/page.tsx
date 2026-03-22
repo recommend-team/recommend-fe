a828893bdb655b90b79ff863b8841ff566080602
jomoartz <johnjohananmogbolu@gmail.com>
2026-03-22 01:19:50 +0100

    feat: sliding locations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import { LandingSectionOne } from "@/components/organisms/LandingSection1";
 import { BackgroundOne } from "@/components/templates/BackgroundOne";
 import NoAppSection from "@/components/templates/NoAppSection";
+import { SlidingLocations } from "@/components/templates/SlidingLocations";
 
 export default function Home() {
   return (
     <BackgroundOne>
       <LandingSectionOne />
       <NoAppSection />
+      <SlidingLocations/>
     </BackgroundOne>
   );
 }
