0045e7153b5676265d166073c6a713fb2579a196
jomoartz <johnjohananmogbolu@gmail.com>
2026-03-28 14:43:49 +0100

    fix: background should not enforce size

--- src/components/templates/BackgroundThree.tsx
+++ src/components/templates/BackgroundThree.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 
 export const BackgroundThree = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="relative min-h-screen w-full overflow-x-hidden">
+    <div className="relative w-full overflow-x-hidden">
       {/* The Background Image */}
       <div className="inset-0 z-10 bg-recommend-amber">
         <Image
