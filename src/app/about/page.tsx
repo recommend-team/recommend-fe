ea7985c24c3a923a63ab875b942d3439eb899bad
Delight Chinecherem <dlghtchenecherem@gmail.com>
2026-04-08 20:42:54 +0100

    feat: add About Us page, fix scrollbar-hide, update neighborhoods to Lagos

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+import GeneralHeader from "@/components/organisms/GeneralHeader";
+import AboutHeroSection from "@/components/templates/AboutHeroSection";
+import FooterSection from "@/components/templates/FooterSection";
+
+export default function AboutPage() {
+  return (
+    <>
+      <GeneralHeader />
+      <AboutHeroSection />
+      <FooterSection />
+    </>
+  );
+}
