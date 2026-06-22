b8a6aef72cd98fb6ad433ce5bac103c42eda86ee
jomoartz <johnjohananmogbolu@gmail.com>
2026-06-22 22:15:56 +0100

    feat:vendor Dashboard layout

---
+++ src/components/templates/DashboardLayout.tsx
@@ -0,0 +1,93 @@
+import {
+  BoxIcon,
+  ChartColumnBig,
+  CheckCircle,
+  ReceiptText,
+  Store,
+} from "lucide-react";
+import { ReactNode, useState } from "react";
+import { Card } from "../atoms/Card";
+import { VendorDashboardTab } from "../molecules/DashboardTab";
+
+export type DashboardTabType = keyof typeof vendorDashboardTabs;
+
+export const vendorDashboardTabs = {
+  dashboard: { icon: BoxIcon, title: "Dashboard" },
+  orders: { icon: CheckCircle, title: "Orders" },
+  inventory: { icon: Store, title: "Inventory" },
+  payments: { icon: ReceiptText, title: "Payments" },
+  grow: { icon: ChartColumnBig, title: "Grow" },
+};
+
+const dashboardvendorDashboardTabs: DashboardTabType[] = [
+  "dashboard",
+  "orders",
+  "inventory",
+  "payments",
+  "grow",
+];
+
+type DashboardLayoutType = {
+  dashboard: ReactNode;
+  orders: ReactNode;
+  inventory: ReactNode;
+  payments: ReactNode;
+  grow: ReactNode;
+};
+
+const DashboardLayout = ({
+  dashboard,
+  orders,
+  inventory,
+  payments,
+  grow,
+}: DashboardLayoutType) => {
+  const [activeTab, setActiveTab] = useState<DashboardTabType>("dashboard");
+
+  const renderTabContent = () => {
+    switch (activeTab) {
+      case "dashboard":
+        return <div>{dashboard}</div>;
+      case "orders":
+        return <div>{orders}</div>;
+      case "inventory":
+        return <div>{inventory}</div>;
+      case "payments":
+        return <div>{payments}</div>;
+      case "grow":
+        return <div>{grow}</div>;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <Card hoverable={false} className="p-0 overflow-hidden">
+      {/* Clickable header */}
+      <div className="w-full bg-[#F6F8FA] h-24 flex items-center">
+        <div className="flex flex-row  h-full justify-center items-center gap-6 pt-2">
+          {dashboardvendorDashboardTabs.map((tab, index) => {
+            const TabIcon = vendorDashboardTabs[tab].icon;
+            const isActive = activeTab === tab;
+
+            return (
+              <VendorDashboardTab
+                TabIcon={TabIcon}
+                activeTab={activeTab}
+                isActive={isActive}
+                setActiveTab={setActiveTab}
+                tab={tab}
+                tabs={vendorDashboardTabs}
+                key={index}
+              />
+            );
+          })}
+        </div>
+      </div>
+      {/* Main Content Area */}
+      <div className="h-50 p-6 vertical-scroll-allow">{renderTabContent()}</div>
+    </Card>
+  );
+};
+
+export { DashboardLayout };
